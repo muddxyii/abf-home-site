cc61d4e608785a9c850ce647ad42d4d5fe4854df
muddxyii <muddxyii@gmail.com>
2024-12-06 17:55:36 -0700

    Remove unused files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Red_Hat_Display } from "next/font/google";
 import React from "react";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 const redHat = Red_Hat_Display({ subsets: ['latin'] })
 
