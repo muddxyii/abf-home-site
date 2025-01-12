38461473d32fe31781887c14cb608946614474d5
Nikolas Padilla <muddxyii@gmail.com>
2025-01-11 23:42:27 -0700

    Updated header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
 import type {Metadata} from "next";
 import "./globals.css";
-import Header from "@/components/Header";
+import Header from "@/components/header";
 import Footer from "@/components/footer";
-import {Red_Hat_Display} from "next/font/google";
 import {Analytics} from "@vercel/analytics/react"
 import {SpeedInsights} from "@vercel/speed-insights/next"
 import React from "react";
-
-const redHat = Red_Hat_Display({subsets: ['latin']})
 
 export const metadata: Metadata = {
     title: "AnyBackflow.com Inc.",
@@ -20,7 +17,7 @@ export default function RootLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en" className={redHat.className}>
+        <html lang="en">
         <body className={`min-h-screen flex flex-col`}>
         <Header/>
         <main className="flex-grow">
---
+++ src/components/header/index.tsx
@@ -0,0 +1,37 @@
+﻿'use client'
+
+import React from "react";
+import {Phone} from "lucide-react";
+import Link from "next/link";
+
+const Header = () => {
+    return (
+        <header
+            className="navbar bg-base-100 flex flex-wrap items-center justify-between px-4"
+            itemScope
+            itemType="http://schema.org/Organization"
+        >
+            <div className="flex-1">
+                <Link href="/" itemProp="name" className="btn btn-ghost normal-case text-xl">
+                    AnyBackflow.com Inc
+                </Link>
+            </div>
+
+            <div className="flex-none flex flex-wrap items-center gap-2 justify-end">
+                <a href="tel:+16022282923"
+                   className="btn btn-primary flex items-center gap-2"
+                   itemProp="telephone"
+                   aria-label="Call us at (602) 228-2923">
+                    <Phone size={20}/>
+                </a>
+                <a href="#contact"
+                   className="btn btn-primary"
+                   aria-label="Contact us for booking">
+                    Book Now
+                </a>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
